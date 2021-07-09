6fa8fedfdfc8dc8678d7a97b51a0b0af92b8472e
Alexagm <alegeo1704@gmail.com>
2021-07-08 23:46:17 -0500

    agregando

---
+++ conexion.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+$host = 'localhost';
+$user = 'root;
+$pass = 'C2EoWAhGqC38j74IlOG4';
+$bd = 'cursocloudcompu';
+$conn = mysqli_connect(
+    $host,
+    $user,
+    $pass,
+    $bd,
+);
+
+?>
